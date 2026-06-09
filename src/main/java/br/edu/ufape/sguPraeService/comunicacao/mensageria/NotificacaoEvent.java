4df91553697a7241fef0f3b0f7f4ca1e5b02d050
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2026-06-09 15:25:53 -0300

    UUID na notificação

--- src/main/java/br/edu/ufape/sguPraeService/comunicacao/mensageria/NotificacaoEvent.java
+++ src/main/java/br/edu/ufape/sguPraeService/comunicacao/mensageria/NotificacaoEvent.java
@@ -4,19 +4,20 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record NotificacaoEvent(
-        UUID destinatarioId,    // ID do usuário Keycloak que vai receber (Pode ser null se for para um grupo)
-        String perfilDestino,   // Ex: "ALUNO", "PROFISSIONAL", "GESTOR" (Útil para envios em massa)
-        String titulo,          // Ex: "Novo Agendamento"
-        String mensagem,        // Ex: "Você tem um novo atendimento marcado para 10/10/2026 às 14:00"
-        String tipo,            // Ex: "AGENDAMENTO", "BENEFICIO", "SISTEMA"
+        UUID id,
+        UUID destinatarioId,
+        String perfilDestino,
+        String titulo,
+        String mensagem,
+        String tipo,
         LocalDateTime dataHoraGeracao
 ) {
-    // Construtor estático para facilitar a criação de notificações para um usuário específico
+
     public static NotificacaoEvent paraUsuario(UUID destinatarioId, String titulo, String mensagem, String tipo) {
-        return new NotificacaoEvent(destinatarioId, null, titulo, mensagem, tipo, LocalDateTime.now());
+        return new NotificacaoEvent(UUID.randomUUID(), destinatarioId, null, titulo, mensagem, tipo, LocalDateTime.now());
     }
 
     public static NotificacaoEvent paraPerfil(String perfilDestino, String titulo, String mensagem, String tipo) {
-        return new NotificacaoEvent(null, perfilDestino, titulo, mensagem, tipo, LocalDateTime.now());
+        return new NotificacaoEvent(UUID.randomUUID(), null, perfilDestino, titulo, mensagem, tipo, LocalDateTime.now());
     }
 }
